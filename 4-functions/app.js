5e6f5bb4a06e3f83ad90a88d1680ffccb54c4d2e
Pavel Pullso <p.dubitskiy@ya.ru>
2020-03-19 10:39:18 +0500

    add homework function and arrays

--- 4-functions/app.js
+++ 4-functions/app.js
@@ -1,10 +1,90 @@
 window.addEventListener('DOMContentLoaded', e => {
   //** Declaration */
+  // function sayHello(firstName, lastName) {
+  //   console.log(firstName, lastName);
+  //   console.log('hello world');
+  //   return `hello ${firstName} ${lastName}`;
+  // }
+  // let res = sayHello('Petya', 'Ivanov');
+  // console.log('res: ', res);
+  // let x = 10;
+  // function foo(x) {
+  //   x = 20;
+  //   console.log(x);
+  //   return x;
+  // }
+  // foo();
+  // console.log(x);
+  // const user = {
+  //   name: 'Denis',
+  //   age: 26
+  // };
+  // function getObj(obj) {
+  //   obj.name = 'Den';
+  // }
+  // getObj(user);
+  // console.log('obj: ', obj);
+  //** Expression */
+  // const square = function(x) {
+  //   return x * x;
+  // };
+  // console.log('square: ', square(4));
+  // (function(msg) {
+  //   console.log(msg);
+  // })('hello World');
+  // function multiply() {
+  //   let ret = 1;
+  //   if (!arguments[0]) return 0;
+  //   for (let i = 0; i < arguments.length; i++) {
+  //     const item = arguments[i];
+  //     console.log('item: ', item, ret);
+  //     ret *= item;
+  //   }
+  //   //console.log(arguments);
+  //   return ret;
+  // }
+  // console.log(multiply(2, 4, 5, 6), multiply());
+  // clgmultiply();
+  // function reverseString(str) {
+  //   str = String(str);
+  //   strNew = '';
+  //   for (i = str.length - 1; i >= 0; i--) {
+  //     strNew += str[i];
+  //   }
+  //   return strNew;
+  // }
+  // console.log(
+  //   reverseString('test'),
+  //   reverseString(''),
+  //   reverseString(null),
+  //   reverseString(undefined),
+  //   reverseString()
+  // );
 
-  function sayHello() {
-    console.log('hello world');
+  // function getCodeStringFromText(str) {
+  //   str = String(str);
+  //   let strNew = '';
+  //   for (let i = 0; i < str.length; i++) {
+  //     strNew += ` ${str[i].charCodeAt()} `;
+  //   }
+  //   strNew.trim();
+  //   return strNew;
+  // }
+
+  // console.log(getCodeStringFromText('hello'));
+
+  function guessTheNumber(num) {
+    if (num < 0 || num > 10)
+      return new Error('Please provide number in range 0-10');
+    if (isNaN(num)) return new Error('Please provide a valid number');
+    let rand = Math.round(Math.random() * 10 + 1);
+    if (rand === num) {
+      return 'YOU WIN';
+    } else {
+      return `You are lose, your Number is ${num}, the random number is ${rand}`;
+    }
   }
+  console.log(guessTheNumber(5));
 
-  sayHello();
   //==== end DOMContentLoaded ====
 });
---
+++ 5-Arrays/app.js
@@ -0,0 +1,90 @@
+window.addEventListener('DOMContentLoaded', e => {
+  //** Базовые методы массиво */
+  // const numArr = [2, 42, 512, 123, 44, 34];
+  // let value;
+  // value = numArr.length;
+  // value = Array.isArray(numArr);
+  // value = numArr[2];
+  // numArr[2] = 42;
+  // value = numArr.indexOf(42);
+  // value = numArr.push(100);
+  // value = numArr.pop();
+  // value = numArr.unshift(24);
+  // value = numArr.shift();
+  // value = numArr.slice(0, 2);
+  // value = numArr.splice(1, 0, 'One', 'two');
+  // value = numArr.reverse();
+  // value = numArr.concat(1, 2);
+  // value = numArr.join('');
+  // value = 'hello world'.split();
+  // value = 'hello world'.split('');
+  // console.log(value, numArr);
+  //**ДЗ */
+  // function getArray(num) {
+  //   let arr = [];
+  //   for (let i = 0; i < num; i++) {
+  //     arr.push(i + 1);
+  //   }
+  //   console.log(arr);
+  //   return arr;
+  // }
+  // getArray(10);
+  // function doubleArray(arr) {
+  //   arr = arr.concat(arr);
+  //   console.log('arr: ', arr);
+  //   return arr;
+  // }
+  // doubleArray([1, 2, 3]);
+
+  // function changeCollection() {
+  //   if (!arguments[0]) return false;
+  //   for (let i = 0; i < arguments.length; i++) {
+  //     if (Array.isArray(arguments[i])) {
+  //       arguments[i].shift();
+  //     }
+  //     console.log(arguments[i]);
+  //   }
+  //   return arguments;
+  // }
+
+  // console.log(changeCollection([1, 2, 3], ['a', 'b', 'c']));
+
+  const users = [
+    {
+      _id: '5e36b779dc76fe3db02adc32',
+      balance: '$1,955.65',
+      picture: 'http://placehold.it/32x32',
+      age: 33,
+      name: 'Berg Zimmerman',
+      gender: 'male'
+    },
+    {
+      _id: '5e36b779d117774176f90e0b',
+      balance: '$3,776.14',
+      picture: 'http://placehold.it/32x32',
+      age: 37,
+      name: 'Deann Winters',
+      gender: 'female'
+    },
+    {
+      _id: '5e36b779daf6e455ec54cf45',
+      balance: '$3,424.84',
+      picture: 'http://placehold.it/32x32',
+      age: 36,
+      name: 'Kari Waters',
+      gender: 'female'
+    }
+  ];
+
+  function filterUsers(arr, key, value) {
+    let ret = [];
+    for (let i = 0; i < arr.length; i++) {
+      let item = arr[i];
+      if (item[key] === value) ret.push(item);
+    }
+    return ret;
+  }
+
+  console.log(filterUsers(users, 'age', 36));
+  //==== end DOMContentLoaded ====
+});
