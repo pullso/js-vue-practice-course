0bf13aaeb1f726d4966df00e1052b475e450e4ba
Pavel Pullso <p.dubitskiy@ya.ru>
2020-04-07 12:06:45 +0500

    add lecture about export and import

--- 26-webpack/src/js/app.js
+++ 26-webpack/src/js/app.js
@@ -1,7 +1,10 @@
-import { conf as c } from './module1';
+import { conf as c, foo } from './module1';
 // import * as mod1 from './module1';
-import Product from './module2';
+// console.log(mod1.conf);
+// mod1.foo();
 
+import Product from './module2';
 console.log(new Product('Apple'));
 console.log(c);
-// console.log(mod1.foo());
+
+//инкапсуляция - организаця приватности данных внутри программы. разделение на уровни доступа.
